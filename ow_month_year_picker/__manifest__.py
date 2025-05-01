5fb76c06dc3020337d521565deec7675f478692b
shirohige007 <farispurnomo35@gmail.com>
2025-05-01 18:14:07 +0700

    fix(month year picker): fix invalid manifest error

--- ow_month_year_picker/__manifest__.py
+++ ow_month_year_picker/__manifest__.py
@@ -11,7 +11,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '17.0.0.1',
+    'version': '18.0.0.1',
     'license': 'LGPL-3',
 	'installable': True,
 
